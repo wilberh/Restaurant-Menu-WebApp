73545c401f68970fb7cd1cb76e9bc0b3a3488623
wilberh <wilberhdez@yahoo.com>
2015-09-06 17:53:32 -0400

    datbase_setup
    Added cascade delete, in case the restaurant gets deleted.  Then the
    rest of the menu items get deleted too.

--- database_setup.py
+++ database_setup.py
@@ -20,6 +20,7 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    menuitems = relationship('MenuItem', cascade='delete, delete-orphan')
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 

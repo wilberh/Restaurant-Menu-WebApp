3e90fcaffa4bca70e2355167a9712e36a4ed6d8f
Wilber Hernandez <wilberhdez@yahoo.com>
2025-05-01 14:58:06 -0400

    adding supporting file
    - requirements
    - wireframes
    - sql statement for inserting data to sqlite db

---
+++ 1__sample_sql_statement.sql
@@ -0,0 +1,9 @@
+-- SQLite
+-- SELECT * FROM user;
+-- INSERT into user (name, email) values ("wilber", "wilber@yourtown.com");
+
+-- INSERT into restaurant (name, user_id) values ("Pieology", 1);
+-- SELECT * from restaurant;
+
+-- INSERT into menu_item (name, description, price, course, restaurant_id, user_id) values ("Margarita", "Best pizza ever", 10.99, "Entree", 1, 1);
+-- SELECT * from menu_item;
